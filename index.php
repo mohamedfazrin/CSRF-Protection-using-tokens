933a4d9416d5f50780a7a902f8aecac1f300d4f4
Mohamed Fazrin <38756788+mohamedfazrin@users.noreply.github.com>
2018-05-28 16:21:20 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+	<?php 
+ 
+    		session_start(); //Starting a session
+
+    		//setting a cookie
+    		$sessionID = session_id(); //storing session id
+
+    		setcookie("session_id",$sessionID,time()+3600,"/","localhost",false,true); 
+    		//cookie will terminate in 3600 seconds (1 hour)
+    
+	?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Assignment 01 - IT15085290</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="text/javascript" src="config.js"> </script>
+
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" method="POST" action="server.php">
+					<span class="login100-form-title p-b-26">
+						Welcome
+					</span>
+					<span class="login100-form-title p-b-48">
+						<i class="zmdi zmdi-font"></i>
+					</span>
+
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="text" name="username">
+						<span class="focus-input100" data-placeholder="Username"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+
+					<div> <input type="hidden" id="csToken" name="CSR"/></div>
+
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn" name="submit" >
+								Login
+							</button>
+						</div>
+					</div>
+					
+					<div class="text-center p-t-115">
+						<span class="txt1">
+							Don’t have an account?
+						</span>
+
+						<a class="txt2" href="#">
+							Sign Up
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendor/select2/select2.min.js"></script>
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src="js/main.js"></script>
+
+
+	<?php 
+	
+	if(isset($_COOKIE['session_id']))
+            { 
+                echo '<script> var token = loadDOC("POST","server.php","csToken");  </script>'; 
+                   
+                //echo "cookie set";     
+            }
+    	?>
+
+
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,51 @@
+<?php
+
+	session_start();
+
+	//create key for CSRF token
+	if(empty($_SESSION['key']))
+	{
+    		$_SESSION['key']=bin2hex(random_bytes(32));
+    
+	}
+
+	//generate CSRF token
+	$token = hash_hmac('sha256',"This is token:index.php",$_SESSION['key']);
+
+	$_SESSION['CSRF'] = $token; //store CSRF token in session variable
+
+	ob_start(); // start of outer buffer function
+
+	echo $token;
+
+
+	if(isset($_POST['submit']))
+	{	
+    		ob_end_clean(); //clean previous displayed echoed  --End of Outer Buffer--
+    
+    		//validate the logins
+    		loginvalidate($_POST['CSR'],$_COOKIE['session_id'],$_POST['username'],$_POST['pass']);
+
+	}	
+
+
+	//function to validate Login
+	function loginvalidate($user_CSRF,$user_sessionID, $username, $password)
+	{
+    		if($username=="abc" && $password=="123" && $user_CSRF==$_SESSION['CSRF'] && $user_sessionID==session_id())
+    		{
+        		echo "<script> alert('Sucessfully loggod in!!') </script>";
+        		echo "Welcome user "."<br/>"; 
+        		echo "For more info visit: ".'<a href="https://fzrsec.blogspot.com/2018/05/cross-site-request-forgery-protection.html", target="_blank" >'. "https://www.fzrsec.blogspot.com"."</a>";
+        		apc_delete('CSRF_token');
+    		}
+    		else
+    		{
+        		echo "<script> alert('Error: failed to Login') </script>";
+        		echo "Login Failed ! "."<br/>"."Authorization Failed!! Please reset!";
+        
+    		}
+	}
+
+
+?>
