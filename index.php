89b1f81d29f8cbc23a27854a7089a5cba92831c5
Mohamed Fazrin <38756788+mohamedfazrin@users.noreply.github.com>
2018-05-28 18:00:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -39,13 +39,10 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<form class="login100-form validate-form" method="POST" action="server.php">
-					<span class="login100-form-title p-b-26">
-						Welcome
-					</span>
-					<span class="login100-form-title p-b-48">
-						<i class="zmdi zmdi-font"></i>
-					</span>
-
+					<div align="center">
+						CSRF Protection Via Double Submitted Cookies
+					</div>
+					<br></br>
 					<div class="wrap-input100 validate-input">
 						<input class="input100" type="text" name="username">
 						<span class="focus-input100" data-placeholder="Username"></span>
@@ -72,11 +69,11 @@
 					
 					<div class="text-center p-t-115">
 						<span class="txt1">
-							Don’t have an account?
+							For more info: 
 						</span>
 
-						<a class="txt2" href="#">
-							Sign Up
+						<a class="txt2" href="https://fzrsec.blogspot.com/2018/05/cross-site-request-forgery-protection_15.html">
+							www.fzrsec.blogspot.com
 						</a>
 					</div>
 				</form>
